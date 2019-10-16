48150167f5a6621a976719dafd5086f5ce70fc58
Amit-Nativ <amitnativ11@gmail.com>
2019-10-16 17:28:55 +0300

    started to add functionality

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,10 +2,12 @@ import React, {useState} from 'react';
 import {Grid, Card, Container, makeStyles, Paper, CardHeader, CardContent} from '@material-ui/core';
 import Display from "./Display";
 import InputLayout from './InputLayout'
+import _ from 'lodash'
 
 const useStyles = makeStyles({
     card: {
-        minWidth: 400
+        maxWidth: 350,
+        padding: '5px'
     },
     mainGrid: {
         backgroundColor: "aqua",
@@ -14,8 +16,51 @@ const useStyles = makeStyles({
 });
 
 export default () => {
-    const [expression, setExpression] = useState([1, '+', 2]);
+    const [expression, setExpression] = useState([1,'+',22]);
     const classes = useStyles();
+
+    const actions = {
+        1: () => addNumber(1),
+        2: () => addNumber(2),
+        3: () => addNumber(3),
+        4: () => addNumber(4),
+        5: () => addNumber(5),
+        6: () => addNumber(6),
+        7: () => addNumber(7),
+        8: () => addNumber(8),
+        9: () => addNumber(9),
+        0: () => addNumber(0),
+        'CE': () => clearLast(),
+        'C': () => clearAll(),
+        '←': () => deleteOne(),
+    };
+
+    const clearAll = () => setExpression([]);
+
+    const clearLast = () => {
+        let newExpression = [...expression];
+        newExpression.splice(-1, 1);
+
+        setExpression([...newExpression]);
+    };
+
+    const deleteOne = () => {
+        const newExpression = [...expression];
+        let last = newExpression.splice(-1, 1);
+
+        if (last < 10) {
+            setExpression([...newExpression]);
+        }
+
+        setExpression([...newExpression, _.toInteger(last / 10)]);
+    };
+
+    const addNumber = (num) => {
+        const newExpression = [...expression];
+        let last = newExpression.splice(-1, 1);
+
+        setExpression([...newExpression, last * 10 + num]);
+    };
 
     return (
         <>
@@ -28,9 +73,9 @@ export default () => {
                 <Grid item></Grid>
                 <Grid item>
                     <Card className={classes.card}>
-                        <CardHeader component={Display} expression={expression}/>
+                        <Display expression={expression}/>
                         <hr/>
-                        <CardContent component={InputLayout}/>
+                        <InputLayout actions={actions}/>
                     </Card>
                 </Grid>
                 <Grid item></Grid>
--- src/Components/InputLayout.js
+++ src/Components/InputLayout.js
@@ -1,7 +1,32 @@
 import React from "react";
+import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button"
 
 export default (props) => {
+
+    const performAction = key => e => {
+        const {actions} = props;
+        actions[key]();
+    };
+
+    const Row = () => {
+        return (
+            <Grid container
+                  justify={"space-between"}>
+                <Button onClick={performAction('←')}>1</Button>
+                <Button>1</Button>
+                <Button>1</Button>
+                <Button>1</Button>
+            </Grid>
+        );
+    }
+
     return (
-        <div>Input</div>
+        <Grid container>
+            <Row></Row>
+            <Row></Row>
+            <Row></Row>
+            <Row></Row>
+        </Grid>
     );
 }
